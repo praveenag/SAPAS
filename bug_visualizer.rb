9c01bf6aeff9028d1950a3673d181b6f005af079
Ashok <ashokk@thoughtworks.com>
2012-03-26 14:20:16 +0530

    Ashok - Changed the red varient for all 20

--- bug_visualizer.rb
+++ bug_visualizer.rb
@@ -8,13 +8,14 @@ class BugVisualizer
 	end
 
 	def commit_count
+		
 		top_file_map = top_culprits
 		base_color = "0000"		
 		nodes = []
 		index=1
 		top_file_map.each do |file_name, file_info|
 			nodes << {"data" => {"File name" => file_name, "playcount" => file_info.bug_count,
-						"$color" =>  "#FF" + (index).to_s * 4, "$area" => file_info.bug_count},
+						"$color" =>  "rgb(#{255 - (index * 10)},0,0)", "$area" => file_info.bug_count},
 			 "id" => file_name.downcase.gsub(' ','_'), 
 			 "name" => file_name.downcase.gsub(' ','_')}			
 			 index = index + 1
